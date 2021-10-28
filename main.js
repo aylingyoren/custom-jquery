49f8333202196cf25001eecbe50b53daf91d3524
aylingyoren <aylingyoren21@gmail.com>
2021-10-28 16:49:16 +0300

    fixed w/o remove method

--- main.js
+++ main.js
@@ -3,70 +3,100 @@ function someTh() {
 }
 
 class JQuery {
-    constructor(elem) {
-      if(typeof elem === 'string') {
-        elem = document.createElement(elem);
+    constructor(elems) {
+      if(typeof elems === 'string') {
+        elems = document.createElement(elems);
       }
-      this.elem = elem;
+      this.elems = [elems];
     }
     text(text) {
-      if(text) {
-          this.elem.textContent = text;
-      } else {
-          console.log(this.elem.textContent);
-          this.elem.textContent;
+      for(let i = 0; i < this.elems.length; i++) {
+        if(text) {
+          this.elems[i].textContent = text;
+        } else {
+          console.log(this.elems.textContent);
+          this.elems[i].textContent;
+        }
       }
       return this;
     }
     css(cssText) {
-      this.elem.style.cssText = `${cssText}`
+      for(let i = 0; i < this.elems.length; i++) {
+        this.elems[i].style.cssText = `${cssText}`;
+      }
       return this;
     }
     append () {
-      document.body.append(this.elem);
+      for(let i = 0; i < this.elems.length; i++) {
+        document.body.append(this.elems[i]);
+      }
       return this;
     }
+  
     remove() {
-        this.elem.remove();
-        return this;
+      for(let i = 0; i < this.elems.length; i++) {
+        this.elems[i].remove();
+      }
+    //   if(this.elems = this.elems[0].children) {
+    //     NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+    //   }
+      return this;
     }
+  
     addClass(className) {
-        this.elem.classList.add(className);
-        return this;
+      for(let i = 0; i < this.elems.length; i++) {
+        this.elems[i].classList.add(className);
+      }
+      return this;
     }
+  
     removeClass(className) {
-        this.elem.classList.remove(className);
-        return this;
-    }
-    attr(name, value) {
-        if(value) {
-            this.elem.setAttribute(name, value);
-            console.log(this.elem.attributes);
-        } else {
-            this.elem.getAttribute(name);
-            console.log(this.elem.attributes);
+      for(let i = 0; i < this.elems.length; i++) {
+        this.elems[i].classList.remove(className);
+        let classAttr = this.elems[i].getAttribute('class');
+        if(!classAttr.value) {
+          this.elems[i].removeAttribute('class');
         }
-        return this;
+      }
+      return this;
+    }
+  
+    attr(name, value) {
+      for(let i = 0; i < this.elems.length; i++) {
+        if(value) {
+            this.elems[i].setAttribute(name, value);
+            console.log(this.elems[i].attributes);
+        } else {
+            this.elems[i].getAttribute(name);
+            console.log(this.elems[i].attributes);
+        }
+      }
+      return this;
     }
     children() {
-        this.elem.children;
-        console.log(this.elem.children);
-        return this;
+      this.elems = this.elems[0].children;
+      return this;
     }
     empty() {
-        this.elem.innerHTML = '';
-        return this;
+      for(let i = 0; i < this.elems.length; i++) {
+        this.elems[i].innerHTML = '';
+      }
+      return this;
     }
     click(handler) {
-        this.elem.addEventListener('click', handler);
-        return this;
+      for(let i = 0; i < this.elems.length; i++) {
+        this.elems[i].addEventListener('click', handler);
+      }
+      return this;
     }
-}
+  }
 
 const ul = document.querySelector('ul');
-const jQuery = new JQuery(document.querySelector('h1'));
-const jQuery2 = new JQuery('h1');
-const jQuery3 = new JQuery(ul);
-jQuery2.text('Big Blue Header').css(`color: blue; font-size: 52px`).addClass('new').removeClass('new').attr('sth', 'yeah').append().children();
-jQuery.text('Big Red Header').css(`color: red; font-size: 52px`).click(someTh);
-jQuery3.children();
+console.log(ul);
+
+const jq = new JQuery(ul);
+jq.children().text('wow').remove();
+
+const h1 = document.querySelector('h1');
+const jgh = new JQuery(h1);
+// jgh.remove();
